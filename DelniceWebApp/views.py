aa9618568e58d9fe870e0677734b8988bfe8645e
EvaErzin <eva.erzin@gmail.com>
2017-05-26 13:45:11 +0200

    Rendering context in index template now works correctly, added logout option.

--- DelniceWebApp/views.py
+++ DelniceWebApp/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import *
+from django.template import RequestContext
 
 # Create your views here.
 def index(request):
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    return render(request, 'index.html')
